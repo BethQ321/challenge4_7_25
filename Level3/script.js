d5ae8ac11ff710f97b6fe3b2a87da5959ecbfb19
bethq321 <bethqualls@comcast.net>
2025-04-16 22:05:08 -0500

    completed third challenge

--- Level3/script.js
+++ Level3/script.js
@@ -4,6 +4,12 @@ const arr1 = [
     ['-', '-', '-']
 ];
 
+const arr1Copy = [
+    ['-', '#', 'F'],
+    ['#', '-', '-'],
+    ['-', '-', '-']
+];;
+
 // should return
 // ['-', '#', '-'],
 // ['#', '-', '-'],
@@ -15,12 +21,25 @@ const arr2 = [
     ['#', '-', '-']
 ];
 
+const arr2Copy = [
+    ['F', 'F', '-'],
+    ['-', '-', '-'],
+    ['#', '-', '-']
+];
+
 // should return
 // ['-', '-', '-'],
 // ['F', 'F', '-'],
 // ['#', '-', '-']
 
 const arr3 = [
+    ['-', 'F', 'F', '-'],
+    ['-', 'F', '-', '-'],
+    ['#', '-', '-', '-'],
+    ['-', '-', '#', '-']
+];
+
+const arr3Copy = [
     ['-', 'F', 'F', '-'],
     ['-', 'F', '-', '-'],
     ['#', '-', '-', '-'],
@@ -40,6 +59,13 @@ const arr4 = [
     ['-', '#', '-', '-']
 ];
 
+const arr4Copy = [
+    ['F', 'F', 'F', '-'],
+    ['-', 'F', '-', '#'],
+    ['-', '-', '-', '-'],
+    ['-', '#', '-', '-']
+];
+
 // should return
 // ['-', '-', '-', '-'],
 // ['F', 'F', 'F', '#'],
@@ -53,6 +79,13 @@ const arr5 = [
     ['#', '-', '-']
 ];
 
+const arr5Copy = [
+    ['-', '-', 'F'],
+    ['-', 'F', 'F'],
+    ['-', 'F', '#'],
+    ['#', '-', '-']
+];
+
 // should return
 // ['-', '-', 'F'],
 // ['-', 'F', 'F'],
@@ -60,6 +93,17 @@ const arr5 = [
 // ['#', '-', '-']
 
 const arr6 = [
+    ['-', '-', 'F', '-'],
+    ['#', '-', 'F', '-'],
+    ['F', 'F', 'F', '-'],
+    ['-', '-', 'F', 'F'],
+    ['-', '-', 'F', '-'],
+    ['#', '-', '-', '-'],
+    ['-', '-', '-', '-'],
+    ['-', '-', '-', '#']
+];
+
+const arr6Copy = [
     ['-', '-', 'F', '-'],
     ['#', '-', 'F', '-'],
     ['F', 'F', 'F', '-'],
@@ -89,20 +133,49 @@ const arr6 = [
 // the connected figure must be connected either horizontally or vertically throughout the array
 // the function should return a 2 dimensional array with the figure dropped as far as possible
 function dropTheF(arr) {
-    // ** code your function here **
-    return "test";
+    while(checkForRoom(arr)) {
+        arr = dropOneLevel(arr);
+    };
+    return arr;
+};
+
+function checkForRoom(arr) {
+    // check the lowest F in each column to see if there are open cells below all of them
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr[i].length; j++) {
+            if(arr[i][j] === 'F') {
+                if((!arr[i + 1]) || (arr[i + 1][j] === '#')) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+};
+
+function dropOneLevel(arr) {
+    // move each F down to the same position in the next row
+    for(let i = arr.length - 1; i > 0; i--) {
+        for(let j = 0; j < arr[i].length; j++) {
+            if(arr[i - 1][j] === 'F') {
+                arr[i][j] = 'F';
+                arr[i - 1][j] = '-';
+            }
+        }
+    }
+    return arr;
 };
 
 // running test cases
 console.log("arr1 input:", arr1);
-console.log("results in:", dropTheF(arr1));
+console.log("results in:", dropTheF(arr1Copy));
 console.log("arr2 input:", arr2);
-console.log("results in:", dropTheF(arr2));
+console.log("results in:", dropTheF(arr2Copy));
 console.log("arr3 input:", arr3);
-console.log("results in:", dropTheF(arr3));
+console.log("results in:", dropTheF(arr3Copy));
 console.log("arr4 input:", arr4);
-console.log("results in:", dropTheF(arr4));
+console.log("results in:", dropTheF(arr4Copy));
 console.log("arr5 input:", arr5);
-console.log("results in:", dropTheF(arr5));
+console.log("results in:", dropTheF(arr5Copy));
 console.log("arr6 input:", arr6);
-console.log("results in:", dropTheF(arr6));
+console.log("results in:", dropTheF(arr6Copy));
